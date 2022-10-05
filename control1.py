0f8dd5694a25fdd2ad2946a67611e34658941cbf
luisllanca <luis.llanca@alumnos.uach.cl>
2022-10-05 15:06:33 -0300

    comentarios

--- control1.py
+++ control1.py
@@ -1,15 +1,14 @@
 def es_anagrama(palabra1,palabra2):
     if len(palabra1) != len(palabra2):
         return False
-    palabra1 = [i for i in palabra1]
-    palabra2 = [i for i in palabra2]
+    palabra1 = [i for i in palabra1] #esto convierte un string a una lista. 
+    palabra2 = [i for i in palabra2] #es lo mismo que hacer palabra1 = list(palabra1)
     palabra1.sort()
     palabra2.sort()
     for i in range(len(palabra1)):
         if palabra1[i] != palabra2[i]:
             return False
     return True
-
 palabras = ["cero","mocasin","lugar","conversacion","cosa","poder","amor"]
 frase = input("Ingresa tu frase: ").split(" ")
 dictsalida = {}
